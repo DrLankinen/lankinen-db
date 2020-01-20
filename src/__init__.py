0274608b3e4f6ccdcf91438686463e3cf8bcfe3f
Elias Lankinen <elias@lankinen.xyz>
2020-01-20 12:13:18 +0200

    Create database skeleton

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqllite:///dbs.db"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
 from src import views
+
+from application.dbs import models
+from application.dbs import views
+
+db.create_all()
