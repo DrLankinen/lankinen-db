f0e1c53999de7db06d33b7b0a617b0aaed0a175b
Elias Lankinen <elias@lankinen.xyz>
2020-01-20 18:35:21 +0200

    week 2 ready

--- src/databases/views.py
+++ src/databases/views.py
@@ -1,14 +1,23 @@
 from src import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from src.databases.models import Database
 
 @app.route("/databases", methods=["GET"])
 def databases_index():
-    return render_template("databases/list.html", tasks = Database.query.all())
+    return render_template("list.html", databases = Database.query.all())
 
 @app.route("/databases/new/")
 def databases_form():
-    return render_template("databases/new.html")
+    return render_template("new.html")
+
+@app.route("/databases/<database_id>/", methods=["POST"])
+def database_change_name(database_id):
+
+    t = Database.query.get(database_id)
+    t.name = "new name"
+    db.session().commit()
+  
+    return redirect(url_for("databases_index"))
 
 @app.route("/databases/", methods=["POST"])
 def databases_create():
