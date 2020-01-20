93f837fe2faa35606aa1b8e6d1c07d2e56012cb7
Elias Lankinen <elias@lankinen.xyz>
2020-01-20 14:58:27 +0200

    sqllite => sqlite

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqllite:///dbs.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dbs.db"
 app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
